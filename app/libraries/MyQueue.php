4636ea9af18bb85d2503c069a4bf4952852b2367
Ro Kleine Sonne <ro@greengoenergy.dk>
2014-12-05 12:12:18 +0100

    back to pheanstalk 2.1

--- app/libraries/MyQueue.php
+++ app/libraries/MyQueue.php
@@ -5,6 +5,7 @@ class MyQueue{
   public function fire($job, $data)
   {
     $query = DB::insert('INSERT INTO queue (created_at) VALUES(NOW())');
+    Log::info('queue job ran as well, nice!');
     $job->delete();
   }
 
